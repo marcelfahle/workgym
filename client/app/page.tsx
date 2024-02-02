99b2730c8d17e6fcab2fce4c3151eb493508ed13
Marcel Fahle <m.fahle@gmail.com>
2024-02-02 14:11:57 +0100

    switches to binary images, adds interval switcher

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -13,15 +13,18 @@ export default function Home() {
   const webcamRef = useRef<Webcam>(null);
   const [avatars, setAvatars] = useState({})
   const sessionId = useMemo(() => nanoid(), []);
+  const [frameRate, setFrameRate] = useState(500);
 
   useEffect(() => {
     socket = io(SERVER_URL);
 
-    socket.on('receive-image', ({ sessionId, image }: { sessionId: string, image: string }) => {
-      console.log('received', image, sessionId)
+    socket.on('receive-image', ({ sessionId, image }: { sessionId: string, image: ArrayBuffer }) => {
+
+      const b = new Blob([image], { type: "image/webp" })
+      const url = URL.createObjectURL(b);
       setAvatars(prevAvatars => ({
         ...prevAvatars,
-        [sessionId]: image
+        [sessionId]: url
       }));
     });
 
@@ -33,13 +36,20 @@ export default function Home() {
   useEffect(() => {
     const interval = setInterval(() => {
       if (webcamRef.current) {
-        const image = webcamRef.current.getScreenshot();
-        socket.emit('send-image', { sessionId, image });
+        const b64 = webcamRef.current.getScreenshot();
+        if (b64) {
+          fetch(b64)
+            .then(res => res.blob())
+            .then(blob => {
+              socket.emit('send-image', { sessionId, image: blob });
+            })
+
+        }
       }
-    }, 500);
+    }, frameRate);
 
     return () => clearInterval(interval);
-  }, [webcamRef]);
+  }, [webcamRef, frameRate]);
 
   return (
     <div className='' >
@@ -54,6 +64,15 @@ export default function Home() {
           width={100}
           videoConstraints={{ width: 100, height: 100 }}
         />
+        <div>
+          <label htmlFor="frameRate">Frame Rate (ms): </label>
+          <select id="frameRate" value={frameRate} onChange={(e) => setFrameRate(Number(e.target.value))}>
+            <option value="100">Very Fast (10 FPS / 100ms)</option>
+            <option value="500">Fast (2 FPS / 500ms)</option>
+            <option value="1000">Normal (1 FPS / 1s)</option>
+            <option value="2000">Slow (0.5 FPS / 2s)</option>
+          </select>
+        </div>
       </div>
       <div style={{ marginTop: 100 }}>
         <h2>Buddies</h2>
